962a2277e21dcc3874f1d328cdbf450a7ae72e10
Carlos Barberena <admin@negotiamini.com>
2022-09-16 21:32:19 -0400

    added readme

---
+++ README.md
@@ -0,0 +1,23 @@
+# Software Engineering Data Science Questions
+
+This repo contains a collection of questions that you will find on a Software Engineering interview. These are publicly available questions you will find on the internet through various sources.
+
+I have taken the liberty to detail as many of the steps possible, even citing verbatim from the source. I have added my own commentary and thoughts on some of the questions.
+
+The solutions provided have been tested and verified to pass. That said, I like many others speak from experience. Memorizing the answers, and not understanding the concepts, and or approach to solving the problem is an exercise in futility.
+
+You only hurt yourself and you perpetuate a malfeseance that hurts all of us. I urge you to take the time to practice as much as possible eventhough you may not undertstand it.
+
+Programming is summarized by the infamous quote:
+
+    "Garbage, in, garbage out"
+
+Lastly, I make no claims to the ownership of any of these questions or solutions. I went with what was the easiest for me to understand and implement. I have done my best to cite the source with both the question and the solution.
+
+If you feel that I may have violated any of your intellectual property rights, just hit me up and I will gladly remove it.
+
+No harm no foul. As an open source advocate, I am ardent in my belief that we must use the information age for good, instead of feverishly and incessecantly trying to elevate our status quo.
+
+Carpe Diem!
+
+
--- findMedian.js
+++ findMedian.js
@@ -1,80 +1,62 @@
-// find median that has the following parameter(s):
+/**
+ * find median that has the following parameter(s):
+ * int arr[n]: an array of integers
+ * 
+ * Returns
+ * 
+ * int: the median of the array
+ * 
+ * Input Format
+ * 
+ * The first line contains the integer, n, the size of arr.
+ * 
+ * The second line contains n space-separated integers arr[i].
+ * 
+ */
 
-// int arr[n]: an array of integers
-
-// Returns
-
-// int: the median of the array
-
-// Input Format
-
-// The first line contains the integer n, the number of elements in the array arr.
-
-// The second line contains n space-separated integers arr[i].
-
-// Constraints
-
-// 1 <= n <= 1000000
-
-// 0 <= arr[i] <= 10^9
-
-// Output Format
+/** Constraints
+ * 
+ * 1 <= n <= 1000000
+ * 
+ * 0 <= arr[i] <= 10^5
+ * 
+ */
 
 // Output the median of the array.
 
-// Sample Input 0
+/**
+ * Sample Input
+ * 
+ *  0 1 2 4 6 5 3
+ * 
+ * Output
+ * 
+ *  3
+ * 
+ * Explanation
+ * 
+ *  The sorted array arr = [0, 1, 2, 3, 4, 5, 6]. 
+ *  It's middle element is at index 3 and the median is 3.
+ * 
+ */
 
-// 6
-// 0 1 2 4 6 5
-
-// Sample Output 0
-
-// 3
-
-// Explanation 0
-
-// The sorted array arr = [0, 1, 2, 4, 5, 6]. It's middle element is at arr[3] and the median is 3.
-
-// Sample Input 1
-
-// 7
-
-// 0 1 2 4 6 5 3
-
-// Sample Output 1
-
-// 3
-
-// Explanation 1
-
-// The sorted array arr = [0, 1, 2, 3, 4, 5, 6]. It's middle element is at arr[3] and the median is 3.
-
-
+let arr = [0, 1, 2, 4, 6, 5, 3];
 
 // Complete the findMedian function below.
 
 function findMedian(arr) {
+  // sort the array first to get the median
+  arr.sort((a, b) => a - b);
 
-    // sort the array
-    arr.sort((a, b) => a - b);
+  // now find the middle index by dividing the length of the array by 2
+  const middleIndex = Math.floor(arr.length / 2);
 
-    // find the middle index
-    const middleIndex = Math.floor(arr.length / 2);
-
-    // return the middle index
-    return arr[middleIndex];
-
+  // return the middle index of the array
+  return arr[middleIndex];
 }
 
+// this must be removed for the test to pass
+findMedian(arr);
 
-
-    
-        arr.sort(function(a, b){return a-b});
-    
-        return arr[Math.floor(arr.length/2)];
-    
-    }
-
-
-
-
+// Solution accepted by HackerRank
+// Used Quokka.js to test the code
