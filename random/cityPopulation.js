a8a8edb964b837f2f4464e4a513c474c97422a42
Carlos Barberena <barberenac@outlook.com>
2022-10-13 15:48:43 -0400

    added cityPopulation challange

--- random/cityPopulation.js
+++ random/cityPopulation.js
@@ -1,63 +1,52 @@
-// count number of people in a city
-// 1. create a function that takes in a city name
-// 2. create a variable that holds the population of the city
-// 3. return the population of the city
-// 4. call the function and pass in a city name
-// 5. print the population of the city to the console
+// // count number of people in a city
+// // 1. create a function that takes in a city name
+// // 2. create a variable that holds the population of the city
+// // 3. return the population of the city
+// // 4. call the function and pass in a city name
+// // 5. print the population of the city to the console
 
-function cityPopulation(city) {
-    let population = 0;
-    switch (city) {
-        case "New York":
-        population = 8550405;
-        break;
-        case "Los Angeles":
-        population = 3971883;
-        break;
-        case "Chicago":
-        population = 2720546;
-        break;
-        case "Houston":
-        population = 2296224;
-        break;
-        case "Philadelphia":
-        population = 1567442;
-        break;
-        case "Phoenix":
-        population = 1563025;
-        break;
-        case "San Antonio":
-        population = 1469845;
-        break;
-        case "San Diego":
-        population = 1394928;
-        break;
-        case "Dallas":
-        population = 1300092;
-        break;
-        case "San Jose":
-        population = 1026908;
-        break;
-        default:
-        population = 0;
-    }
-    return population;
-    }
-    console.log(cityPopulation("New York"));
+// function cityPopulation(city) {
+//     let population = 0;
+//     switch (city) {
+//         case "New York":
+//         population = 8550405;
+//         break;
+//         case "Los Angeles":
+//         population = 3971883;
+//         break;
+//         case "Chicago":
+//         population = 2720546;
+//         break;
+//         case "Houston":
+//         population = 2296224;
+//         break;
+//         case "Philadelphia":
+//         population = 1567442;
+//         break;
+//         case "Phoenix":
+//         population = 1563025;
+//         break;
+//         case "San Antonio":
+//         population = 1469845;
+//         break;
+//         case "San Diego":
+//         population = 1394928;
+//         break;
+//         case "Dallas":
+//         population = 1300092;
+//         break;
+//         case "San Jose":
+//         population = 1026908;
+//         break;
+//         default:
+//         population = 0;
+//     }
+//     return population;
+//     }
+//     console.log(cityPopulation("New York"));
 
 // Language: javascript
 // calculate cities total population by state
-
-function calculateCitiesTotalPopulationByState(state) {
-    let totalPopulation = 0;
-    for (let i = 0; i < cities.length; i++) {
-        if (cities[i].state === state) {
-            totalPopulation += cities[i].population;
-        }
-    }
-    return totalPopulation;
-}
-console.log(calculateCitiesTotalPopulationByState("CA"));
 
 
 /**
@@ -119,42 +108,67 @@ let state = {
     }
 }
 
+// actual response
 
-function calculateCitiesTotalPopulationByState(state) {
-    let totalPopulation = 0;
-    for (let i = 0; i < state.cities.length; i++) {
-        totalPopulation += state.cities[i].population;
-    }
-    return totalPopulation;
-}
-console.log(calculateCitiesTotalPopulationByState(state.CA));
-
-// Language: javascript
-// calculate cities total population by state
-
-function calculateCitiesTotalPopulationByState(state) {
-    let totalPopulation = 0;
-    for (let i = 0; i < cities.length; i++) {
-        if (cities[i].state === state) {
-            totalPopulation += cities[i].population;
+function calculatePopulationByStatesCities(state) {
+    for (let key in state) {
+        let population = 0;
+        for (let i = 0; i < state[key].cities.length; i++) {
+            population += state[key].cities[i].population;
         }
+        state[key].population = population;
     }
-    return totalPopulation;
+    return state;
 }
-console.log(calculateCitiesTotalPopulationByState("CA"));
+console.log(calculatePopulationByStatesCities(state));
 
-// Language: javascript
-// calculate cities total population by state
+// trying second way
 
-function calculateCitiesTotalPopulationByState(state) {
-    let totalPopulation = 0;
-    for (let i = 0; i < cities.length; i++) {
-        if (cities[i].state === state) {
-            totalPopulation += cities[i].population;
-        }
-    }
-    return totalPopulation;
-}
-console.log(calculateCitiesTotalPopulationByState
-("CA"));
-function calculateTotalCityPopulation(state)
+// function calculateCitiesTotalPopulationByState(state) {
+//     let totalPopulation = 0;
+//     for (let i = 0; i < cities.length; i++) {
+//         if (cities[i].state === state) {
+//             totalPopulation += cities[i].population;
+//         }
+//     }
+//     return totalPopulation;
+// }
+// console.log(calculateCitiesTotalPopulationByState("CA"));
+
+// function calculateCitiesTotalPopulationByState(state) {
+//     let totalPopulation = 0;
+//     for (let i = 0; i < state.cities.length; i++) {
+//         totalPopulation += state.cities[i].population;
+//     }
+//     return totalPopulation;
+// }
+// console.log(calculateCitiesTotalPopulationByState(state.CA));
+
+// // Language: javascript
+// // calculate cities total population by state
+
+// function calculateCitiesTotalPopulationByState(state) {
+//     let totalPopulation = 0;
+//     for (let i = 0; i < cities.length; i++) {
+//         if (cities[i].state === state) {
+//             totalPopulation += cities[i].population;
+//         }
+//     }
+//     return totalPopulation;
+// }
+// console.log(calculateCitiesTotalPopulationByState("CA"));
+
+// // // Language: javascript
+// // // calculate cities total population by state
+
+// // function calculateCitiesTotalPopulationByState(state) {
+// //     let totalPopulation = 0;
+// //     for (let i = 0; i < cities.length; i++) {
+// //         if (cities[i].state === state) {
+// //             totalPopulation += cities[i].population;
+// //         }
+// //     }
+// //     return totalPopulation;
+// // }
+// // console.log(calculateCitiesTotalPopulationByState
+// // ("CA"));
